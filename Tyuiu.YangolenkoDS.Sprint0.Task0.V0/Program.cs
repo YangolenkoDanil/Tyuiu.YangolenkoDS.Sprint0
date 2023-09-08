181a2b0ddbfdb4bfe422aa995b1b37496611433e
Tyuiu.YangolenkoDS@outlook.com <angolenkodanil@gmail.com>
2023-09-08 20:48:11 +0500

    Task1

--- Tyuiu.YangolenkoDS.Sprint0.Task0.V0/Program.cs
+++ Tyuiu.YangolenkoDS.Sprint0.Task0.V0/Program.cs
@@ -10,6 +10,11 @@ namespace Tyuiu.YangolenkoDS.Sprint0.Task0.V0
     {
         static void Main(string[] args)
         {
+            string name = ("Данил");
+            int age = 18;
+            Console.WriteLine("Hello, World");
+            Console.WriteLine("Здравствуйте, меня зовут " + name + "мне " + age + " лет !");
+            Console.ReadKey();
         }
     }
 }

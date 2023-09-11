529e2fc24246f7cbf46ae8a73cc80e76681cd1b2
Tyuiu.YangolenkoDS@outlook.com <angolenkodanil@gmail.com>
2023-09-11 11:34:26 +0500

    Task2

---
+++ Tyuiu.YangolenkoDS.Sprint0.Task2.V0.Test/DataServiceTest.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.YangolenkoDS.Sprint0.Task2.V0.Lib;
+namespace Tyuiu.YangolenkoDS.Sprint0.Task2.V0.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckGetMessageValid()
+        {
+            var name = "Данил";
+            var res = DataService.GetMessage(name);
+
+            Assert.AreEqual("Привет..., Данил", res);
+        }
+    }
+}
---
+++ Tyuiu.YangolenkoDS.Task2.V0/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.YangolenkoDS.Sprint0.Task2.V0.Lib;
+namespace Tyuiu.YangolenkoDS.Task2.V0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DataService.GetMessage("Данил"));
+            Console.ReadKey();
+        }
+    }
+}
